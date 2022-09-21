e35632ce2302995b501ecf6c36f10ee11c0ea275
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-09-22 00:09:47 +0800

    chore：新增git ignore规则，添加pinia依赖

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
-import './scss/style.scss'
-import naiveui from 'naive-ui'
-import App from './App.vue'
-import router from './router/index'
+import { createApp } from "vue";
+import "./scss/style.scss";
+import naiveui from "naive-ui";
+import App from "./App.vue";
+import router from "./router/index";
+import { createPinia } from "pinia";
 
+const pinia = createPinia();
 const app = createApp(App);
 app.use(naiveui);
 app.use(router);
-app.mount('#app');
+app.use(pinia);
+app.mount("#app");
