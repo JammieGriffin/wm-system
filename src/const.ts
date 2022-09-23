10204439013393b88eabf1c35bc13d22f30c8970
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-09-24 05:13:01 +0800

    feat：新增axios拦截器

--- src/const.ts
+++ src/const.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
-
+import { useUsrstore } from "./store";
+const store = useUsrstore();
 const axiosConf = {
   baseURL: "http://localhost:8188",
   timeout: 5000,
-  Headers: {
+  headers: {
     "Content-Type": "application/json",
   },
 };
+const baseAxios = axios.create(axiosConf);
 
-export const baseAxios = axios.create(axiosConf);
+baseAxios.interceptors.request.use((conf) => {
+  if (conf.url !== "/sys/login") {
+    conf.headers!.usrToken = store.$state.token;
+  }
+  return conf;
+});
+baseAxios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  }
+);
+export { baseAxios };
