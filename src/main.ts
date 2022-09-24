753ebd8bf5d88e4757d9d9c6a3519d90b11685fc
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-09-25 04:06:48 +0800

    refactor：修改pinia的引入方式

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import "./scss/style.scss";
 import naiveui from "naive-ui";
 import App from "./App.vue";
 import router from "./router/index";
-import { createPinia } from "pinia";
+import { pinia } from "./store";
 
-const pinia = createPinia();
+
+
 const app = createApp(App);
-app.use(naiveui);
-app.use(router);
-app.use(pinia);
+app.use(pinia)
+.use(naiveui)
+.use(router);
+
 app.mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,12 @@
-import { defineStore } from "pinia";
+import { defineStore,createPinia } from "pinia";
 import { IUsr } from "../api/data";
-
+export const pinia = createPinia();
 export const useUsrstore = defineStore("usr", {
   state: () => {
     return {
       token: "" as string,
       usr: {} as IUsr,
+      invalidLogin:false,
     };
   },
 });
